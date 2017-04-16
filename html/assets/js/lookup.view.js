fe0f886a44f316f5175f5726d02656fc9f48f681
wstlabs <notyet@notyet.org>
2017-04-15 20:43:34 -0400

    raise maxZoom

--- html/assets/js/lookup.view.js
+++ html/assets/js/lookup.view.js
@@ -22,7 +22,8 @@ lookup.view.initMap = function(mapname) {
     });
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 20
     }).addTo(map);
 
     L.control.zoom({
