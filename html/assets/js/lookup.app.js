9beb865e519682dc83c0631e11c93be845fe373d
wstlabs <notyet@notyet.org>
2017-05-31 11:13:20 -0400

    bulk indent

--- html/assets/js/lookup.app.js
+++ html/assets/js/lookup.app.js
@@ -1,15 +1,17 @@
 (function() {
-lookup.app = {};
-lookup.app.run = function() {
-    lookup.log(1,'version = '+lookup.version);
-    $(document).ready(function () {
-        lookup.view.initGoogleAutocomplete();
-        lookup.control.initHandlers();
-        lookup.view.initMap('themap');
-        lookup.view.initPopups();
-        lookup.log(1,'happy');
-    });
-    $.ajaxSetup(lookup.config.ajaxsetup);
-    lookup.log(1,'launched.');
-};
+
+    lookup.app = {};
+    lookup.app.run = function() {
+        lookup.log(1,'version = '+lookup.version);
+        $(document).ready(function () {
+            lookup.view.initGoogleAutocomplete();
+            lookup.control.initHandlers();
+            lookup.view.initMap('themap');
+            lookup.view.initPopups();
+            lookup.log(1,'happy');
+        });
+        $.ajaxSetup(lookup.config.ajaxsetup);
+        lookup.log(1,'launched.');
+    };
+
 })();
