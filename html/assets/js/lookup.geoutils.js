7220ac29730785f45a2949e5bab4f90df9584c73
wstlabs <notyet@notyet.org>
2017-06-02 11:47:35 -0400

    bulk indent

--- html/assets/js/lookup.geoutils.js
+++ html/assets/js/lookup.geoutils.js
@@ -1,126 +1,124 @@
 // requires: lookup.utils
 (function() {
 
-lookup.geoutils = {}; 
+    lookup.geoutils = {}; 
 
-// Takes a string representing a (perhaps multi-part) city name,
-// and perhaps two-letter street abbreviation (or not), e.g.: 
-//
-//   "Brooklyn NY", "Port Richmond NY", "Long Island City" 
-//
-// and does its best to split into an dict of (city,state).
-// Our assumption is that the final "word" in this string will 
-// be a state abbreviation iff it's two letters in length.
-lookup.geoutils.splitCity = function(s) {
-  var t = lookup.utils.strip(s).split(/\s+/);
-  var n = t.length;
-  if (n < 1) { return null; }
-  if (n === 1) { return {'city':t[0],'state':null}; }
-  if (t[n-1].length === 2)  {
-    var s = t.join(' ');
-    var city = s.substr(0,s.length-3)
-    return {'city':city,'state':t[n-1]};
-  }
-  else  {
-    var city = t.join(' ');
-    return {'city':city,'state':null};
-  }
-};
+    // Takes a string representing a (perhaps multi-part) city name,
+    // and perhaps two-letter street abbreviation (or not), e.g.: 
+    //
+    //   "Brooklyn NY", "Port Richmond NY", "Long Island City" 
+    //
+    // and does its best to split into an dict of (city,state).
+    // Our assumption is that the final "word" in this string will 
+    // be a state abbreviation iff it's two letters in length.
+    lookup.geoutils.splitCity = function(s) {
+        var t = lookup.utils.strip(s).split(/\s+/);
+        var n = t.length;
+        if (n < 1) { return null; }
+        if (n === 1) { return {'city':t[0],'state':null}; }
+        if (t[n-1].length === 2)  {
+            var s = t.join(' ');
+            var city = s.substr(0,s.length-3)
+            return {'city':city,'state':t[n-1]};
+        }  else  {
+            var city = t.join(' ');
+            return {'city':city,'state':null};
+        }
+    };
 
-// Anticipates an address in one of 5 basic formats, give or take
-// whitespace:
-//
-//   590 Bushwick Ave
-//   590 Bushwick Ave, Brooklyn
-//   590 Bushwick Ave, Brooklyn NY
-//   590 Bushwick Ave, Brooklyn, NY
-//   590 Bushwick Ave, Brooklyn NY, United States
-//
-// And returns a dict of canonicalized components.  Some notes:
-//
-//  - The latter is the format produced by Google Autocomplete.
-//    So while no user would type the "United States" part, it's going
-//    to be present in most submissions, so we need to accomodate it.
-//
-//  - In any case we don't care about the US part or the state part;
-//    just the house_number, street_name, and city. 
-//
-//  - Also, to be clear, we aren't interpreting the "city" part as any 
-//    particular borough designation at this point; that determination
-//    gets made at the backend.
-//
-// For the time being we're assuming entries won't contain zip codes; 
-// but if they do, out approach (not yet implemented) will be to simply 
-// strip them out.  But since Autocomplete doesn't provide them and users 
-// are unlikely to type them, we won't worry about them just yet.
-//
-// Finally:
-//
-// If we end up creating a degenerate response (with an insufficient set 
-// of fields), then an 'error' member is added to the response dict; the
-// presence of this field is a boolean indicator of parsing status.
-//
-// And the initial raw address (after being stripped of leading and
-// trailing whitspace) is returned also, for diagnostic purposes. 
-//
-lookup.geoutils.processAddress = function(rawaddr) {
-  var r = {};
-  r.rawaddr = lookup.utils.strip(rawaddr); 
-  var terms = r.rawaddr.split(/\s*,\s*/);
-  var re = new RegExp(/^(\S+)\s+(.*)$/);
-  var m = re.exec(terms[0]);
-  if (m) {
-    r.house_number = m[1]; 
-    r.street_name = m[2].replace(/\s+/,' ');
-  }
-  if (terms.length > 1)  {
-      r.city = lookup.geoutils.splitCity(terms[1]).city;
-  }
-  // Do some basic error diagnostics on the way out.
-  // Multiple error conditions are possible, but we prioritize on what's 
-  // most likely to be left unset before submission. 
-  if ((r.house_number === undefined) || (r.street_name === undefined))  {
-    r.error = 'Invalid address (need at least a house number and street name)';
-  }
-  return r 
-};
+    // Anticipates an address in one of 5 basic formats, give or take
+    // whitespace:
+    //
+    //   590 Bushwick Ave
+    //   590 Bushwick Ave, Brooklyn
+    //   590 Bushwick Ave, Brooklyn NY
+    //   590 Bushwick Ave, Brooklyn, NY
+    //   590 Bushwick Ave, Brooklyn NY, United States
+    //
+    // And returns a dict of canonicalized components.  Some notes:
+    //
+    //  - The latter is the format produced by Google Autocomplete.
+    //    So while no user would type the "United States" part, it's going
+    //    to be present in most submissions, so we need to accomodate it.
+    //
+    //  - In any case we don't care about the US part or the state part;
+    //    just the house_number, street_name, and city. 
+    //
+    //  - Also, to be clear, we aren't interpreting the "city" part as any 
+    //    particular borough designation at this point; that determination
+    //    gets made at the backend.
+    //
+    // For the time being we're assuming entries won't contain zip codes; 
+    // but if they do, out approach (not yet implemented) will be to simply 
+    // strip them out.  But since Autocomplete doesn't provide them and users 
+    // are unlikely to type them, we won't worry about them just yet.
+    //
+    // Finally:
+    //
+    // If we end up creating a degenerate response (with an insufficient set 
+    // of fields), then an 'error' member is added to the response dict; the
+    // presence of this field is a boolean indicator of parsing status.
+    //
+    // And the initial raw address (after being stripped of leading and
+    // trailing whitspace) is returned also, for diagnostic purposes. 
+    //
+    lookup.geoutils.processAddress = function(rawaddr) {
+        var r = {};
+        r.rawaddr = lookup.utils.strip(rawaddr); 
+        var terms = r.rawaddr.split(/\s*,\s*/);
+        var re = new RegExp(/^(\S+)\s+(.*)$/);
+        var m = re.exec(terms[0]);
+        if (m) {
+            r.house_number = m[1]; 
+            r.street_name = m[2].replace(/\s+/,' ');
+        }
+        if (terms.length > 1)  {
+            r.city = lookup.geoutils.splitCity(terms[1]).city;
+        }
+        // Do some basic error diagnostics on the way out.
+        // Multiple error conditions are possible, but we prioritize on what's 
+        // most likely to be left unset before submission. 
+        if ((r.house_number === undefined) || (r.street_name === undefined))  {
+            r.error = 'Invalid address (need at least a house number and street name)';
+        }
+        return r 
+    };
    
-lookup.geoutils.boroName = { 
-    1:"Manhattan", 2:"Bronx", 3:"Brooklyn", 4:"Queens", 5:"Staten Island"
-};
+    lookup.geoutils.boroName = { 
+        1:"Manhattan", 2:"Bronx", 3:"Brooklyn", 4:"Queens", 5:"Staten Island"
+    };
 
+    // Unpacks a BBL to a dict of named components. 
+    lookup.geoutils.splitBBL = function(bbl) {
+        var blockandlot = bbl % 1000000000;
+        var r = {};
+        r.lot = blockandlot % 10000;
+        r.block = (blockandlot - r.lot) / 10000;
+        r.boroID = (bbl - blockandlot) / 1000000000
+        r.boroName = lookup.geoutils.boroName[r.boroID]; 
+        return r; 
+    };
 
-// Unpacks a BBL to a dict of named components. 
-lookup.geoutils.splitBBL = function(bbl) {
-  var blockandlot = bbl % 1000000000;
-  var r = {};
-  r.lot = blockandlot % 10000;
-  r.block = (blockandlot - r.lot) / 10000;
-  r.boroID = (bbl - blockandlot) / 1000000000
-  r.boroName = lookup.geoutils.boroName[r.boroID]; 
-  return r; 
-};
+    lookup.geoutils.genMapBox = function(geolat,geolon,mapw,maph) {
+        var r = {};
+        r.sx = (geolon - mapw).toFixed(4);
+        r.ex = (geolon + mapw).toFixed(4);
+        r.sy = (geolat - maph).toFixed(4);
+        r.ey = (geolat + maph).toFixed(4);
+        return r; 
+    };
 
-lookup.geoutils.genMapBox = function(geolat,geolon,mapw,maph) {
-  var r = {};
-  r.sx = (geolon - mapw).toFixed(4);
-  r.ex = (geolon + mapw).toFixed(4);
-  r.sy = (geolat - maph).toFixed(4);
-  r.ey = (geolat + maph).toFixed(4);
-  return r; 
-};
-
-lookup.geoutils.genMapURL = function(geolat,geolon) {
-  var mapw = 0.00125;
-  var maph = 0.00075;
-  var mapbox = lookup.geoutils.genMapBox(geolat,geolon,mapw,maph);
-  lookup.log(2,':: mapbox = '+mapbox);
-  var mx = geolat.toFixed(4);
-  var my = geolon.toFixed(4);
-  var boxtup = mapbox.sx+','+mapbox.sy+','+mapbox.ex+','+mapbox.ey;
-  var marker = mx+','+my;
-  var suburl = '/export/embed.html?bbox='+boxtup+'&layer=mapnik&marker='+marker;
-  return 'http://www.openstreetmap.org' + suburl
-};
+    lookup.geoutils.genMapURL = function(geolat,geolon) {
+        var mapw = 0.00125;
+        var maph = 0.00075;
+        var mapbox = lookup.geoutils.genMapBox(geolat,geolon,mapw,maph);
+        lookup.log(2,':: mapbox = '+mapbox);
+        var mx = geolat.toFixed(4);
+        var my = geolon.toFixed(4);
+        var boxtup = mapbox.sx+','+mapbox.sy+','+mapbox.ex+','+mapbox.ey;
+        var marker = mx+','+my;
+        var suburl = '/export/embed.html?bbox='+boxtup+'&layer=mapnik&marker='+marker;
+        return 'http://www.openstreetmap.org' + suburl
+    };
 
 })();
--- html/assets/js/lookup.utils.js
+++ html/assets/js/lookup.utils.js
@@ -1,82 +1,82 @@
 // 
-// Generic string utils and such.
+// Generic string/url munging utils and such.
 // 
 (function() {
 
-lookup.utils = {}
+    lookup.utils = {}
 
-// Strips leading and trailing whitespace from a string. 
-lookup.utils.strip = function(s) {
-  return s.replace(/^\s+/,'').replace(/\s+$/,'');
-};
+    // Strips leading and trailing whitespace from a string. 
+    lookup.utils.strip = function(s) {
+        return s.replace(/^\s+/,'').replace(/\s+$/,'');
+    };
 
-// We're not happy with what encodeURIComponent does to typical URL paths
-// (as it encodes way too many symbols that aren't strictly necessary to 
-// encode).  All we really need is the space-to-plus substitution for now.
-lookup.utils.encode_nicely = function(path) {
-    return lookup.utils.replaceAll(path,' ','+'); 
-};
+    // We're not happy with what encodeURIComponent does to typical URL paths
+    // (as it encodes way too many symbols that aren't strictly necessary to 
+    // encode).  All we really need is the space-to-plus substitution for now.
+    lookup.utils.encode_nicely = function(path) {
+        return lookup.utils.replaceAll(path,' ','+'); 
+    };
 
-lookup.utils.replaceAll = function(s,u,v) {
-  while(s.indexOf(u) > 0)  {
-    s = s.replace(u,v);
-  }
-  return s
-};
+    lookup.utils.replaceAll = function(s,u,v) {
+        while(s.indexOf(u) > 0)  {
+            s = s.replace(u,v);
+        }
+        return s
+    };
 
-/* Where's lpad when you need it? */
-lookup.utils.lpad = function(s,c,n) {
-  s = ''+s;
-  while (s.length < n)  { s = c + s; }
-  return s;
-};
+    /* Where's lpad when you need it? */
+    lookup.utils.lpad = function(s,c,n) {
+        s = ''+s;
+        while (s.length < n)  { s = c + s; }
+        return s;
+    };
 
 
-/* The following 3 functions convert a BBL to a query string (or fragement thereof)
-   suitable for embedding into links for external services as noted.  If it can't convert
-   properly, then an error string is returned which will at least bubble up into the
-   application context so you can eventually find out what went wrong. */
+    /* The following 3 functions convert a BBL to a query string (or fragement thereof)
+       suitable for embedding into links for external services as noted.  If it can't convert
+       properly, then an error string is returned which will at least bubble up into the
+       application context so you can eventually find out what went wrong. */
 
-// For taxbills.nyc; e.g.  4000440023 => '4/00044/0023' 
-lookup.utils.bbl2slug = function(bbl) {
-  if (bbl !== undefined)  {
-    var _lpad = lookup.utils.lpad;
-    var t = lookup.geoutils.splitBBL(bbl);
-    return '' + t.boroID + '/' + _lpad(t.block,'0',5) + '/' + _lpad(t.lot,'0',4);
-  } else { return 'invalid-bbl-string'; }
-};
+    // For taxbills.nyc; e.g.  4000440023 => '4/00044/0023' 
+    lookup.utils.bbl2slug = function(bbl) {
+        if (bbl !== undefined)  {
+            var _lpad = lookup.utils.lpad;
+            var t = lookup.geoutils.splitBBL(bbl);
+            return '' + t.boroID + '/' + _lpad(t.block,'0',5) + '/' + _lpad(t.lot,'0',4);
+        } else { return 'invalid-bbl-string'; }
+    };
 
-// for ACRIS:  4000440023 -> 'borough=4&block=44&lot=23'
-lookup.utils.bbl2acris = function(bbl) {
-  if (bbl !== undefined)  {
-    var t = lookup.geoutils.splitBBL(bbl);
-    return 'borough=' + t.boroID + '&block=' + t.block + '&lot=' + t.lot;
-  } else { return 'invalid-bbl-string'; }
-};
+    // for ACRIS:  4000440023 -> 'borough=4&block=44&lot=23'
+    lookup.utils.bbl2acris = function(bbl) {
+        if (bbl !== undefined)  {
+        var t = lookup.geoutils.splitBBL(bbl);
+        return 'borough=' + t.boroID + '&block=' + t.block + '&lot=' + t.lot;
+        } else { return 'invalid-bbl-string'; }
+    };
 
-// for DOB:  4000440023 -> 'boro=4&block=44&lot=23'
-lookup.utils.bbl2bisweb = function(bbl) {
-  if (bbl !== undefined)  {
-    var t = lookup.geoutils.splitBBL(bbl);
-    return 'boro=' + t.boroID + '&block=' + t.block + '&lot=' + t.lot;
-  } else { return 'invalid-bbl-string'; }
-};
+    // for DOB:  4000440023 -> 'boro=4&block=44&lot=23'
+    lookup.utils.bbl2bisweb = function(bbl) {
+        if (bbl !== undefined)  {
+            var t = lookup.geoutils.splitBBL(bbl);
+            return 'boro=' + t.boroID + '&block=' + t.block + '&lot=' + t.lot;
+        } else { return 'invalid-bbl-string'; }
+    };
 
-// Adapted from https://stackoverflow.com/questions/901115/ (under CC).
-//
-// Returns the requested query string parameter by name from the given URL, using 
-// the current window location as a default).  Doesn't work for multi-valued params 
-// (i.e. repeated instances ofa  given name), but that's OK.  But has advantage
-// of simplicity and a likely high degree of backward compatibility.
-//
-lookup.utils.getQueryStringParam = function(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-};
+    // Adapted from https://stackoverflow.com/questions/901115/ (under CC).
+    //
+    // Returns the requested query string parameter by name from the given URL, using 
+    // the current window location as a default).  Doesn't work for multi-valued params 
+    // (i.e. repeated instances ofa  given name), but that's OK.  But has advantage
+    // of simplicity and a likely high degree of backward compatibility.
+    //
+    lookup.utils.getQueryStringParam = function(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    };
 
 })();
