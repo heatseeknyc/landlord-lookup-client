3afe0c8917d62605d805e1f1bacc03f7379021ee
wstlabs <notyet@notyet.org>
2017-07-10 22:15:11 -0400

    tidy whitespace

--- html/assets/js/dusty.js
+++ html/assets/js/dusty.js
@@ -1,10 +1,26 @@
 // A few shortucts for dustjs.
 (function () {
-    dusty = {};
-    dusty.version = '001a';
 
-    dusty.render = function() {
-        console.log('dusty!');
+    dusty = {};
+    dusty.version = '001b';
+
+    // Compiles + loads a template under the given key, in one op. 
+    // Expects a template to exist under the id '#tmpl-key'.
+    dusty.load = function(key) {
+        // console.log('dusty.load '+key+' ..');
+        var source   = $('#tmpl-'+key).html();
+        var compiled = dust.compile(source, key);
+        dust.loadSource(compiled);
+    };
+
+    dusty.render = function(divname,object) {
+        // console.log('dusty.render '+divname+' ..');
+        // console.log(object);
+        var tmplkey = divname;
+        dust.render(tmplkey, object, function(err, out) {
+              $('#'+divname).html(out);
+        });
+        console.log('dusty.render '+divname+' done');
     };
 
 })();
